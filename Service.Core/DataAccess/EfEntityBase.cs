402f41a47528fb279b4c87960c23eee027bcc196
Ogün Ergin <ogun.ergin35@gmail.com>
2022-10-20 14:53:38 +0300

    Added entityrepository

---
+++ Service.Core/DataAccess/EfEntityBase.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Core.DataAccess
+{
+    public class EfEntityBase<TEntity, TContext> : IEntityRepository<TEntity>
+        where TEntity : class, new()
+        where TContext : DbContext, new()
+    {
+        public void Add(TEntity entity)
+        {
+            using (var context = new TContext())
+            {
+                var addedEntity = context.Entry(entity);
+                addedEntity.State = EntityState.Added;
+                context.SaveChanges();
+            }
+        }
+
+        public void Delete(TEntity entity)
+        {
+            using (var context = new TContext())
+            {
+                var addedEntity = context.Entry(entity);
+                addedEntity.State = EntityState.Deleted;
+                context.SaveChanges();
+            }
+        }
+
+        public TEntity Get(Expression<Func<TEntity, bool>> filter, Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>? include = null)
+        {
+            using (var context = new TContext())
+            {
+                var quaryable = context.Set<TEntity>().AsQueryable();
+                if (include != null)
+                {
+                    quaryable = include(quaryable);
+                }
+                return quaryable.FirstOrDefault(filter);
+            }
+        }
+
+        public async Task<List<TEntity>> GetAllAsync(Expression<Func<TEntity, bool>> filter = null, Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>? include = null)
+        {
+            using (var context = new TContext())
+            {
+                var quaryable = context.Set<TEntity>().AsQueryable();
+                if (include != null)
+                {
+                    quaryable = include(quaryable);
+                }
+                return filter == null
+                    ? await quaryable.ToListAsync()
+                    : await quaryable.Where(filter).ToListAsync();
+            }
+        }
+
+        public async Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> filter, Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>? include = null)
+        {
+            using (var context = new TContext())
+            {
+                var quaryable = context.Set<TEntity>().AsQueryable();
+                if (include != null)
+                {
+                    quaryable = include(quaryable);
+                }
+                return await quaryable.FirstOrDefaultAsync(filter);
+            }
+        }
+
+        public void Update(TEntity entity)
+        {
+            using (var context = new TContext())
+            {
+                var addedEntity = context.Entry(entity);
+                addedEntity.State = EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- Service.Core/DataAccess/IEntityRepository.cs
+++ Service.Core/DataAccess/IEntityRepository.cs
@@ -1,12 +1,20 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore.Query;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Service.Core.DataAccess
 {
-    public interface IEntityRepository
+    public interface IEntityRepository<T> where T : class, new()
     {
+        void Add(T entity);
+        void Delete(T entity);
+        void Update(T entity);
+        Task<List<T>> GetAllAsync(Expression<Func<T, bool>> filter = null, Func<IQueryable<T>, IIncludableQueryable<T, object>>? include = null);
+        Task<T> GetAsync(Expression<Func<T, bool>> filter, Func<IQueryable<T>, IIncludableQueryable<T, object>>? include = null);
+        T Get(Expression<Func<T, bool>> filter, Func<IQueryable<T>, IIncludableQueryable<T, object>>? include = null);
     }
 }
